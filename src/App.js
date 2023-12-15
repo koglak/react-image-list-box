b1ddaf1541a0fb5d5b2300de71ac9d9de722abd2
Kardelen Oğlakcıoğlu Özen <koglakk@gmail.com>
2023-12-15 15:02:30 +0300

    initial commit for folder organization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import ImageBox from './components/ImageBox'
+import './styles/App.css'
+import { imagesData } from './data/imagesData';
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h5>React Image List Box</h5>
+      {/* <ImageBox images={imagesData} /> */}
     </div>
   );
 }
