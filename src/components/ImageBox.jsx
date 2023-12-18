52c6f1b95134d61c6bc78272432f4eabca4ed2cf
Kardelen Oğlakcıoğlu Özen <koglakk@gmail.com>
2023-12-18 16:14:52 +0300

    ImageGrid converted to component

--- src/components/ImageBox.jsx
+++ src/components/ImageBox.jsx
@@ -1,82 +1,30 @@
 import React, { useState } from 'react';
-import '../styles/ImageBox.css';
-
-import { RiDeleteBin5Line } from "react-icons/ri";
 
 import Pagination from './Pagination';
 import Settings from './Settings'
+import ImageGrid from './ImageGrid';
 
 const ImageBox = ({ images, setImages, perPage, imageWidth, imageHeight, boxWidth, boxHeight, enableCheckBox, enableDelete }) => {
   const [currentPage, setCurrentPage] = useState(1);
-
-  // Calculate the indices for the current page's images
-  const indexOfLastImage = currentPage * perPage;
-  const indexOfFirstImage = indexOfLastImage - perPage;
-  const currentImages = images.slice(indexOfFirstImage, indexOfLastImage);
-
   const [selectedImages, setSelectedImages] = useState([]);
-
-  const toggleSelectImage = (imageName) => {
-    setSelectedImages(prevSelectedImages => {
-      if (prevSelectedImages.includes(imageName)) {
-        return prevSelectedImages.filter(name => name !== imageName);
-      } else {
-        return [...prevSelectedImages, imageName];
-      }
-    });
-  };
-
-  const handleDelete = (indexToDelete) => {
-    const actualIndex = indexOfFirstImage + indexToDelete;
-    const filteredImages = images.filter((_, index) => index !== actualIndex);
-    const imageNameToDelete = images[actualIndex].filename; // Get the name of the image to delete
-  
-    setImages(filteredImages);
-  
-    setSelectedImages(prevSelectedImages => 
-      prevSelectedImages.filter(name => name !== imageNameToDelete)
-    );
-  
-    if (currentPage > 1 && filteredImages.length <= indexOfFirstImage) {
-      setCurrentPage(currentPage - 1);
-    }
-  };
-  
 
   return (
     <div className='image-box' style={{ width: `${boxWidth}px`, height: `${boxHeight}px` }}>
       <Settings numSelectedImages={selectedImages.length} />
 
-      <div className="image-grid">
-        {currentImages.map((image, index) => (
-          <div key={index} className="image-item" >
-            {
-              enableCheckBox && <input
-                type="checkbox"
-                className="image-checkbox"
-                checked={selectedImages.includes(image.filename)}
-                onChange={() => toggleSelectImage(image.filename)}
-                id={`checkbox-${index}`}
-              />
-            }
-
-            {enableDelete && <button
-              className="image-item-button"
-              onClick={() => handleDelete(index)}
-              title="Delete Image"
-            >
-              <RiDeleteBin5Line size={20} />
-            </button>}
-
-            <img
-              src={image.src}
-              alt={image.filename}
-              style={{ width: `${imageWidth}px`, height: `${imageHeight}px` }}
-            />
-            <div className='image-label'>{image.filename}</div>
-          </div>
-        ))}
-      </div>
+      <ImageGrid
+        images={images}
+        setImages={setImages}
+        perPage={perPage}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        imageWidth={imageWidth}
+        imageHeight={imageHeight}
+        enableCheckBox={enableCheckBox}
+        enableDelete={enableDelete}
+        selectedImages={selectedImages}
+        setSelectedImages={setSelectedImages}
+      />
 
       <Pagination
         numOfPage={Math.ceil(images.length / perPage)}
---
+++ src/components/ImageGrid.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { RiDeleteBin5Line } from "react-icons/ri";
+import '../styles/ImageBox.css'
+
+const ImageGrid = ({ images, setImages, perPage, currentPage, setCurrentPage, imageWidth, imageHeight, enableCheckBox, enableDelete, selectedImages, setSelectedImages }) => {
+
+    const indexOfLastImage = currentPage * perPage;
+    const indexOfFirstImage = indexOfLastImage - perPage;
+    const currentImages = images.slice(indexOfFirstImage, indexOfLastImage);
+
+    const toggleSelectImage = (imageName) => {
+        setSelectedImages(prevSelectedImages => {
+            if (prevSelectedImages.includes(imageName)) {
+                return prevSelectedImages.filter(name => name !== imageName);
+            } else {
+                return [...prevSelectedImages, imageName];
+            }
+        });
+    };
+
+    const handleDelete = (imageNameToDelete) => {
+        const filteredImages = images.filter(image => image.filename !== imageNameToDelete);
+        setImages(filteredImages);
+        setSelectedImages(prevSelectedImages =>
+            prevSelectedImages.filter(name => name !== imageNameToDelete)
+        );
+
+        const totalNumberOfPages = Math.ceil(filteredImages.length / perPage);
+        if (currentPage > totalNumberOfPages) {
+            setCurrentPage(totalNumberOfPages);
+        }
+    };
+
+    return (
+        <div className="image-grid">
+            {currentImages.slice(0, perPage).map((image, index) => (
+                <div key={index} className="image-item">
+                    {enableCheckBox && (
+                        <input
+                            type="checkbox"
+                            className="image-checkbox"
+                            checked={selectedImages.includes(image.filename)}
+                            onChange={() => toggleSelectImage(image.filename)}
+                            id={`checkbox-${index}`}
+                        />
+                    )}
+
+                    {enableDelete && (
+                        <button
+                            className="image-item-button"
+                            onClick={() => handleDelete(image.filename)}
+                            title="Delete Image"
+                        >
+                            <RiDeleteBin5Line size={20} />
+                        </button>
+                    )}
+
+                    <img
+                        src={image.src}
+                        alt={image.filename}
+                        style={{ width: `${imageWidth}px`, height: `${imageHeight}px` }}
+                    />
+                    <div className='image-label'>{image.filename}</div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default ImageGrid;
