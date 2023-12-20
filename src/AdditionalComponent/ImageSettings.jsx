9ed34f114731a8cd41e8d279935e4f618c865e60
Kardelen Oğlakcıoğlu Özen <koglakk@gmail.com>
2023-12-20 12:47:12 +0300

    zindex issue fix due to checkbox

--- src/AdditionalComponent/ImageSettings.jsx
+++ src/AdditionalComponent/ImageSettings.jsx
@@ -3,7 +3,7 @@ import { IoMdClose } from "react-icons/io";
 
 function ImageSettings({ showOnClickPage, setShowOnClickPage, selectedImgObj }) {
     return (
-        <div className='w-100 h-100 bg-white' style={{ position: 'absolute', top: "0px", left: "0px", opacity: "1" }}>
+        <div className='w-100 h-100 bg-white' style={{ position: 'absolute', top: "0px", left: "0px", opacity: "1",  zIndex: 1000 }}>
             <div
                 className='bg-light w-100 d-flex justify-content-between p-3'
                 style={{ cursor: "pointer" }}
