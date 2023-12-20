1aa8f29dd85e8ca58bd61777904481583b403876
Kardelen Oğlakcıoğlu Özen <koglakk@gmail.com>
2023-12-20 16:47:51 +0300

    more image added

--- src/data/imagesData.js
+++ src/data/imagesData.js
@@ -67,4 +67,109 @@ export const imagesData = [
         type: "",
         annotations: []
     },
+    {
+        src: 'https://cdn.britannica.com/72/234472-050-735B6214/maltese-dog.jpg',
+        filename: '11.jpg',
+        categories: [],
+        type: "",
+        annotations: []
+    },
+    {
+        src: 'https://images.pexels.com/photos/35638/labrador-breed-dogs-animal.jpg?cs=srgb&dl=pexels-pixabay-35638.jpg&fm=jpg',
+        filename: '12.jpg',
+        categories: [],
+        type: "",
+        annotations: []
+    },
+    {
+        src: 'https://plus.unsplash.com/premium_photo-1665952050053-31ac47c6ff4b?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y3V0ZSUyMGRvZ3xlbnwwfHwwfHx8MA%3D%3D',
+        filename: '13.jpg',
+        categories: [],
+        type: "",
+        annotations: []
+    },
+    {
+        src: 'https://www.pitpat.com/wp-content/uploads/2020/07/Dog_-rights_MS_breeds_outdoors_stationary__two-dogs-sitting-in-flowers_-labrador_@fergusthelab_-1024x769.jpg',
+        filename: '14.jpg',
+        categories: [],
+        type: "",
+        annotations: []
+    },
+    {
+        src: 'https://www.southernliving.com/thmb/Rz-dYEhwq_82C5_Y9GLH2ZlEoYw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/gettyimages-837898820-1-4deae142d4d0403dbb6cb542bfc56934.jpg',
+        filename: '15.jpg',
+        categories: [],
+        type: "",
+        annotations: []
+    },
+    {
+        src: 'https://www.thesprucepets.com/thmb/wDwU14vPAAGa6sl9V0hdIrJggpI=/3600x0/filters:no_upscale():strip_icc()/cute-dog-breeds-we-can-t-get-enough-of-4589340-hero-04aba92c6fbb4651b7fa1f54823a1a6d.jpg',
+        filename: '16.jpg',
+        categories: [],
+        type: "",
+        annotations: []
+    },
+    {
+        src: 'https://media-cldnry.s-nbcnews.com/image/upload/rockcms/2022-08/220805-border-collie-play-mn-1100-82d2f1.jpg',
+        filename: '17.jpg',
+        categories: [],
+        type: "",
+        annotations: []
+    },
+    {
+        src: 'https://media.npr.org/assets/img/2022/11/23/russian-toy-2-002--059b8a825dac13f92234d65777e6b29b0914e92f-s1100-c50.jpg',
+        filename: '18.jpg',
+        categories: [],
+        type: "",
+        annotations: []
+    },
+    {
+        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Cute_dog.jpg/640px-Cute_dog.jpg',
+        filename: '19.jpg',
+        categories: [],
+        type: "",
+        annotations: []
+    },
+    {
+        src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSybbFFIg828nU0-z4JtjC2Z3j-NcsHpI740LAPa-LkA7YdmpviuM9OGaCuqJvtvbkuEf0&usqp=CAU',
+        filename: '20.jpg',
+        categories: [],
+        type: "",
+        annotations: []
+    },
+    {
+        src: 'https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/wp-content/uploads/2023/05/great_dane.jpeg.jpg',
+        filename: '21.jpg',
+        categories: [],
+        type: "",
+        annotations: []
+    },
+    {
+        src: 'https://cdn.mos.cms.futurecdn.net/ASHH5bDmsp6wnK6mEfZdcU.jpg',
+        filename: '22.jpg',
+        categories: [],
+        type: "",
+        annotations: []
+    },
+    {
+        src: 'https://image.petmd.com/files/styles/863x625/public/2022-10/iStock-1301465193_0.jpg',
+        filename: '23.jpg',
+        categories: [],
+        type: "",
+        annotations: []
+    },
+    {
+        src: 'https://cdn-prd.content.metamorphosis.com/wp-content/uploads/sites/6/2022/02/shutterstock_1568771329-768x512.jpg',
+        filename: '24.jpg',
+        categories: [],
+        type: "",
+        annotations: []
+    },
+    {
+        src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQy3xh5thd9vTa9jF15PwlubByfJxrv1cpMUg&usqp=CAU',
+        filename: '25.jpg',
+        categories: [],
+        type: "",
+        annotations: []
+    },
 ]
