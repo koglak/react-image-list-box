775e72cce3e1dcf8398767c401c434bcc3fe5930
Kardelen Oğlakcıoğlu Özen <koglakk@gmail.com>
2023-12-20 15:31:55 +0300

    select-deselect styling

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -8,8 +8,10 @@ import ImageContext from '../context/ImageContext';
 import '../styles/ClassificationPopUp.css';
 import '../styles/Settings.css'
 
-import { FaSearch } from "react-icons/fa";
+import { FaSearch, FaCheck } from "react-icons/fa";
 import { IoImagesOutline } from "react-icons/io5";
+import { IoMdClose } from "react-icons/io";
+
 
 function Settings() {
     const [isClassVisible, setIsClassVisible] = React.useState(false)
@@ -64,15 +66,28 @@ function Settings() {
                         </button>))
                 }
 
-                <button className='btn btn-dark ms-1' disabled={selectedImages.length === filteredImages.length} onClick={() => setSelectedImages(filteredImages.map(img => img.filename))}>Select All</button>
-                <button className='btn btn-dark ms-1' disabled={selectedImages.length === 0} onClick={() => setSelectedImages([])}>De-Select</button>
 
             </div>}
-            {enableCheckBox && <div className='text-end m-1' style={{ fontSize: "12px" }}>
-                <span>{selectedImages.length} Images Selected</span>
+
+
+            {enableCheckBox && <div className='m-1 d-flex align-items-center ' style={{ fontSize: "12px" }}>
+                <button className='select-tag-text'
+                    disabled={selectedImages.length === filteredImages.length}
+                    onClick={() => setSelectedImages(filteredImages.map(img => img.filename))}>
+                    <FaCheck /> Select All
+                </button>
+
+                <div className={selectedImages.length > 0 ? 'number-selected-text-colored' : 'number-selected-text'}>{selectedImages.length} Images Selected</div>
+
+                <button className="select-tag-text"
+                    disabled={selectedImages.length === 0}
+                    onClick={() => setSelectedImages([])}>
+                    <IoMdClose />De-Select
+                </button>
+
             </div>}
 
-            <div className='d-flex align-items-center' style={{height: "48px"}}>
+            <div className='d-flex align-items-center' style={{ height: "48px" }}>
                 <div className="input-group w-25" >
                     <input type="text"
                         className="text-input"
@@ -85,7 +100,7 @@ function Settings() {
                 </div>
 
                 {enableCheckBox && <DropdownComponent title="Tag" options={tagList} keyName="categories" />}
-                {enableCheckBox && <DropdownComponent title="Type" options={imageTypes.map(t=>t.name)} keyName="type" />}
+                {enableCheckBox && <DropdownComponent title="Type" options={imageTypes.map(t => t.name)} keyName="type" />}
 
                 <ClassificationPopUp setIsClassVisible={setIsClassVisible} isClassVisible={isClassVisible} tagList={tagList} />
 
